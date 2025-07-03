c32c6dddde6932b1dfb774ab49219fe2266815cb
istekhar <istekhararkam25@gmail.com>
2025-07-03 22:46:13 +0530

    signup done

---
+++ src/components/Select.jsx
@@ -0,0 +1,24 @@
+import React, { useId } from "react";
+
+function Select({ options, label, className, ...props }, ref) {
+  const id = useId();
+  return (
+    <div className="w-full">
+      {label && <label htmlFor={id} className=""></label>}
+      <select
+        id={id}
+        {...props}
+        ref={ref}
+        className={`w-full px-3 py-2 text-black duration-200 bg-white border border-gray-200 rounded-lg outline-none focus:bg-gray-50 ${className}`}
+      >
+        {options?.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default React.forwardRef(Select);
